13c6fdd18c8fcb6c244dd8679457ed62f86d9f70
hyein <hyein2072@gmail.com>
2022-05-31 16:10:03 +0900

    [FIX] SSL 인증서 문제 해결

--- data/src/main/java/com/yapp/android2/data/service/Service.kt
+++ data/src/main/java/com/yapp/android2/data/service/Service.kt
@@ -1,17 +1,26 @@
 package com.yapp.android2.data.service
 
 import com.yapp.android2.data.service.convert.BestFriendResponseConvertFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 interface Service {
     companion object {
-        private const val BASE_URL = "http://61.79.96.183:8080/"
+        private const val BASE_URL = "https://61.79.96.183:8443/"
+        private const val HOST_NAME = "61.79.96.183"
 
         internal fun retroBuilder(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(getOkhttpClient())
                 .addConverterFactory(BestFriendResponseConvertFactory.create())
                 .build()
         }
+
+        private fun getOkhttpClient() = OkHttpClient.Builder().apply {
+            hostnameVerifier { hostname, _ ->
+                hostname!!.contentEquals(HOST_NAME)
+            }
+        }.build()
     }
 }
