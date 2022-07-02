2cb6b8ee7fbab82920e0822054dd1f482ede6122
taeseongyun <yts0646@gmail.com>
2022-07-02 19:39:38 +0900

    [ Feature ] : preference 저장된 데이터에 따라 SettingActivity 뷰 세팅

--- presentation/settings/src/main/java/com/yapp/android2/settings/SettingViewModel.kt
+++ presentation/settings/src/main/java/com/yapp/android2/settings/SettingViewModel.kt
@@ -1,12 +1,30 @@
 package com.yapp.android2.settings
 
+import androidx.lifecycle.viewModelScope
 import com.best.friends.core.BaseViewModel
+import com.yapp.android2.domain.repository.setting.SettingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel @Inject constructor(
+    settingUserData: SettingsUserDataParseFactory
+) : BaseViewModel() {
 
-): BaseViewModel(){
+    val user = settingUserData.getUser()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = SettingRepository.Settings.Init,
+            started = SharingStarted.WhileSubscribed(5000L)
+        )
 
+    fun logout() {
+        //TODO
+    }
+
+    fun onUserWithDraw() {
+        //TODO
+    }
 }
--- presentation/settings/src/main/java/com/yapp/android2/settings/SettingsActivity.kt
+++ presentation/settings/src/main/java/com/yapp/android2/settings/SettingsActivity.kt
@@ -1,16 +1,44 @@
 package com.yapp.android2.settings
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.best.friends.core.BaseActivity
+import com.best.friends.core.ui.showToast
+import com.yapp.android2.domain.repository.setting.SettingRepository
 import com.yapp.android2.settings.databinding.ActivitySettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.*
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
-
+@AndroidEntryPoint
 class SettingsActivity : BaseActivity<ActivitySettingsBinding>(R.layout.activity_settings) {
+    private val viewModel by viewModels<SettingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         overridePendingTransition(R.anim.activity_in_transition, R.anim.activity_stay_transition)
+
+        viewModel.user.flowWithLifecycle(lifecycle = this.lifecycle)
+            .filter{ it != SettingRepository.Settings.Init }
+            .onEach {
+                when(it) {
+                    is SettingRepository.Settings.Error -> { showToast("유저 정보가 없습니다.") }
+                    is SettingRepository.Settings.Success -> { binding.viewInit(it) }
+                    else -> Unit
+                }
+            }
+            .launchIn(lifecycleScope)
+    }
+
+    private fun ActivitySettingsBinding.viewInit(value: SettingRepository.Settings.Success) {
+        tvUserId.text = value.email
+        tvCreatedAt.text = getString(R.string.created_at, value.createAt.format(DateTimeFormatter.ofPattern("yy.MM.dd")))
     }
 
 
