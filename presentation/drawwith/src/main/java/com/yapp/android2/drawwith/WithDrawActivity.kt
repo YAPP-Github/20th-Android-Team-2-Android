6ea17835149d2952b41017c14b9b1b659295583f
taeseongyun <yts0646@gmail.com>
2022-07-05 13:23:03 +0900

    [ Feature ] : 회원탈퇴 액티비티 생성

---
+++ presentation/drawwith/src/main/java/com/yapp/android2/drawwith/WithDrawActivity.kt
@@ -0,0 +1,11 @@
+package com.yapp.android2.drawwith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WithDrawActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_with_draw)
+    }
+}
