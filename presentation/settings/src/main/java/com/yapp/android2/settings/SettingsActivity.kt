46d40c64f9dab10fb944aea0ce88e24bf63387bf
Leo Lee <lee2928030@naver.com>
2022-07-22 00:35:05 +0900

    fix: 설정화면 항목의 터치영역 수정

--- presentation/settings/src/main/java/com/yapp/android2/settings/SettingsActivity.kt
+++ presentation/settings/src/main/java/com/yapp/android2/settings/SettingsActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.best.friends.core.BaseActivity
-import com.best.friends.core.setOnSingleClickListener
 import com.best.friends.core.extensions.showToast
+import com.best.friends.core.setOnSingleClickListener
 import com.best.friends.navigator.LogoutNavigator
 import com.best.friends.navigator.WebViewNavigator
 import com.best.friends.navigator.WithDrawNavigator
@@ -36,34 +36,47 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding>(R.layout.activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         overridePendingTransition(R.anim.activity_in_transition, R.anim.activity_stay_transition)
 
         binding.setOnClickListener()
 
         viewModel.user.flowWithLifecycle(lifecycle = this.lifecycle)
-            .filter{ it != SettingRepository.Settings.Init }
+            .filter { it != SettingRepository.Settings.Init }
             .onEach {
-                when(it) {
-                    is SettingRepository.Settings.Error -> { showToast("유저 정보가 없습니다.") }
-                    is SettingRepository.Settings.Success -> { binding.viewInit(it) }
+                when (it) {
+                    is SettingRepository.Settings.Error -> {
+                        showToast("유저 정보가 없습니다.")
+                    }
+                    is SettingRepository.Settings.Success -> {
+                        binding.viewInit(it)
+                    }
                     else -> Unit
                 }
             }
             .launchIn(lifecycleScope)
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.activity_out_transition, R.anim.activity_stay_transition)
+    }
+
     private fun ActivitySettingsBinding.viewInit(value: SettingRepository.Settings.Success) {
         tvUserId.text = value.email
-        tvVersion.text = packageManager.getPackageInfo(this@SettingsActivity.packageName, 0).versionName
-        tvCreatedAt.text = getString(R.string.setting_created_at, LocalDateTime.parse(value.createAt)?.format(DateTimeFormatter.ofPattern("yy.MM.dd")))
+        tvVersion.text =
+            packageManager.getPackageInfo(this@SettingsActivity.packageName, 0).versionName
+        tvCreatedAt.text = getString(
+            R.string.setting_created_at,
+            LocalDateTime.parse(value.createAt)?.format(DateTimeFormatter.ofPattern("yy.MM.dd"))
+        )
     }
 
     private fun ActivitySettingsBinding.setOnClickListener() {
-        ivBack.setOnSingleClickListener(500) {
+        ivBack.setOnSingleClickListener(200) {
             finish()
         }
-        tvPolicy.setOnSingleClickListener(500) {
+
+        viewPolicy.setOnSingleClickListener(200) {
             this@SettingsActivity.startActivity(navigator.intent(this@SettingsActivity))
         }
 
@@ -72,12 +85,10 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding>(R.layout.activity
         }
 
         viewUser.setOnSingleClickListener {
-            startActivity(withDrawNavigator.intent(this@SettingsActivity).putExtra(EMAIL, binding.tvUserId.text))
+            startActivity(
+                withDrawNavigator.intent(this@SettingsActivity)
+                    .putExtra(EMAIL, binding.tvUserId.text)
+            )
         }
-    }
-
-    override fun onDestroy() {
-        overridePendingTransition(R.anim.activity_out_transition, R.anim.activity_stay_transition)
-        super.onDestroy()
     }
 }
